b2ca21c808b09447b1d1eb674ca9837ac852b315
premyennam <128230752+premyennam@users.noreply.github.com>
2023-04-12 19:49:14 +0530

    Merge all lists into 1sortedlinkedlist & return it

---
+++ Merge k Sorted Lists.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        lst=[]
+        for i in lists:
+            while i:
+                lst.append(i.val)
+                i=i.next
+        lst.sort()
+        a=ListNode(0)
+        tmp=a
+        for i in lst:
+            tmp.next=ListNode(i)
+            tmp=tmp.next
+        return a.next
